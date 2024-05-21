866c5c6263424756f436826d14f9a620c1aa28b1
lsy8181 <a4630486@gmail.com>
2024-05-21 15:34:51 +0900

    ex2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const students = [
@@ -17,18 +17,28 @@ function App() {
   ];
 
   const [minAge, setMinAge] = useState(18);
+  const [filteredStudents, setFilteredStudents] = useState([]);
 
-  // TODO: filter를 사용하여 minAge 이상의 학생들만 선택하세요.
-  // const filteredStudents = ...;
+  useEffect(() => {
+    setFilteredStudents(students.filter((student) => student.age >= minAge));
+  }, [minAge]);
 
   return (
     <div>
       <h1>학생 목록</h1>
       다음 나이 이상의 학생목록만 출력해요 :{" "}
-      {/* TODO: input에 입력된 값(숫자) 이상의 나이를 가진 학생들만 출력하세요. */}
-      <input type="number" /> 살 이상
+      <input
+        type="number"
+        value={minAge}
+        onChange={(e) => setMinAge(Number(e.target.value))}
+      />{" "}
+      살 이상
       <ul>
-        {/* TODO: map을 사용하여 필터링된 학생들의 정보를 표시하세요. 동명이인은 없다고 가정합니다. ex) 홍길동 - Age: 24, Grade: A */}
+        {filteredStudents.map((student) => (
+          <li key={student.name}>
+            {`${student.name} - Age: ${student.age}, Grade: ${student.grade}`}
+          </li>
+        ))}
       </ul>
     </div>
   );
